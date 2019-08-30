84b9238669dd93873786bdc8efb3dd4f4371f589
songwenhao <wenhao.song@meixinfinance.com>
2019-08-30 14:23:10 +0800

    init

--- mq-consumer/src/main/java/com/song/consume/service/KfkConsumeService.java
+++ mq-consumer/src/main/java/com/song/consume/service/KfkConsumeService.java
@@ -15,7 +15,8 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class KfkConsumeService {
 
-    @KafkaListener(topics = {"song_test_1"}, groupId = "local_consumer_1", containerFactory = "kafkaListenerContainerFactory")
+    @KafkaListener(topics = {"song_test_1"}, groupId = "local_consumer_1",
+            containerFactory = "kafkaListenerContainerFactory")
     public void listenSongTest(ConsumerRecord<Object, Object> data, Acknowledgment ack) {
         log.info("topic->{},data->{}", "song_test_1", JSON.toJSONString(data));
         Msg msg = JSON.parseObject(data.value().toString(), Msg.class);
